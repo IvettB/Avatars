ea988e727aa161908fe93b784e82a6cc00e9bc15
Ishmael Taleo <ishmaeltaleo@gmail.com>
2023-05-26 11:21:10 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/avatar_demo1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.avatar_demo1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int imageIndex = 0;
+  List<String> images = [
+    'lib/images/happy_vegetable.png',
+    'lib/images/sad_vegetable.png',
+  ];
+
+  List<String> images2 = [
+    'lib/images/mean_fruit.png',
+    'lib/images/shocked_fruit.png',
+  ];
+
+  late List<List<String>> allImages;
+  int currentListIndex = 0;
+
+  _MyAppState() {
+    allImages = [images, images2];
+  }
+
+  void changeImage() {
+    setState(() {
+      imageIndex = (imageIndex + 1) % allImages[currentListIndex].length;
+    });
+  }
+
+  void changeAvatar() {
+    setState(() {
+      imageIndex = 0; // Set the image index to 0
+      currentListIndex = (currentListIndex + 1) % allImages.length; // Switch between the lists
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Avatar Demo',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Avatar Demo'),
+        ),
+        body: Container(
+          alignment: Alignment.topCenter,
+          padding: const EdgeInsets.only(top: 100),
+          child: Column(
+            children: [
+              AnimatedSwitcher(
+                duration: const Duration(milliseconds: 500),
+                transitionBuilder: (Widget child, Animation<double> animation) {
+                  return FadeTransition(
+                    opacity: animation,
+                    child: child,
+                  );
+                },
+                child: CircleAvatar(
+                  key: ValueKey<String>(allImages[currentListIndex][imageIndex]),
+                  radius: 80,
+                  backgroundImage: AssetImage(allImages[currentListIndex][imageIndex]),
+                ),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: changeImage,
+                child: Text('Change Image'),
+              ),
+              ElevatedButton(
+                onPressed: changeAvatar,
+                child: Text('Change Avatar'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
