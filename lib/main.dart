6f1f35295d148393d6b739c30fba1971b0a112d0
tmont5 <montgomerytanner40@gmail.com>
2023-06-22 14:53:09 -0600

    rough go at black eye

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,14 @@ class _MyAppState extends State<MyApp> {
   int imageIndex5 = 0;
   int imageIndex6 = 0;
   int imageIndex7 = 0;
+  int imageIndex8 = 0;
   bool areGlassesVisible = false;
   bool isCrownVisible = false;
   bool isPimpleVisible = false;
   bool isEarringVisible = false;
   bool isEyePatchVisible = false;
   bool isNeckBraceVisible = false;
+  bool isBlackEyeVisible = false;
   List<String> avatars = [
     'lib/images/people/1.png',
     'lib/images/people/2.png',
@@ -74,6 +76,10 @@ class _MyAppState extends State<MyApp> {
     'lib/images/neckbraces/1.png',
   ];
 
+  List<String> blackeyes = [
+    'lib/images/blackeyes/1.png',
+  ];
+
   late List<List<String>> allImages;
   int currentListIndex = 0;
 
@@ -123,6 +129,12 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  void changeBlackEye() {
+    setState(() {
+      imageIndex8 = (imageIndex8 + 1) % blackeyes.length;
+    });
+  }
+
   void toggleGlasses() {
     setState(() {
       areGlassesVisible = !areGlassesVisible;
@@ -156,6 +168,12 @@ class _MyAppState extends State<MyApp> {
   void toggleNeckBrace() {
     setState(() {
       isNeckBraceVisible = !isNeckBraceVisible;
+    });
+  }
+
+  void toggleBlackEye() {
+    setState(() {
+      isBlackEyeVisible = !isBlackEyeVisible;
     });
   }
 
@@ -242,18 +260,6 @@ class _MyAppState extends State<MyApp> {
                     ),
                   ),
                   AnimatedOpacity(
-                    opacity: isEyePatchVisible ? 1.0 : 0.0,
-                    duration: Duration(milliseconds: 500),
-                    child: Visibility(
-                      visible: isEyePatchVisible,
-                      child: Transform.translate(
-                        offset: Offset(-30.0, -10.0),
-                        child: Image.asset(eyepatches[imageIndex6],
-                            width: 75, height: 75),
-                      ),
-                    ),
-                  ),
-                  AnimatedOpacity(
                     opacity: isNeckBraceVisible ? 1.0 : 0.0,
                     duration: Duration(milliseconds: 500),
                     child: Visibility(
@@ -262,6 +268,30 @@ class _MyAppState extends State<MyApp> {
                         offset: Offset(0.0, 50.0),
                         child: Image.asset(neckbraces[imageIndex7],
                             width: 250, height: 250),
+                      ),
+                    ),
+                  ),
+                  AnimatedOpacity(
+                    opacity: isBlackEyeVisible ? 1.0 : 0.0,
+                    duration: Duration(milliseconds: 500),
+                    child: Visibility(
+                      visible: isBlackEyeVisible,
+                      child: Transform.translate(
+                        offset: Offset(-13.0, -2.0),
+                        child: Image.asset(blackeyes[imageIndex8],
+                            width: 100, height: 100),
+                      ),
+                    ),
+                  ),
+                  AnimatedOpacity(
+                    opacity: isEyePatchVisible ? 1.0 : 0.0,
+                    duration: Duration(milliseconds: 500),
+                    child: Visibility(
+                      visible: isEyePatchVisible,
+                      child: Transform.translate(
+                        offset: Offset(-30.0, -10.0),
+                        child: Image.asset(eyepatches[imageIndex6],
+                            width: 75, height: 75),
                       ),
                     ),
                   ),
@@ -280,14 +310,19 @@ class _MyAppState extends State<MyApp> {
                 onPressed: changeCrowns,
                 child: Text('Change Crown'),
               ),
-              ElevatedButton(
+              /*ElevatedButton(
                 onPressed: changePimples,
                 child: Text('Change Pimple'),
-              ),
+              ),*/
               ElevatedButton(
                 onPressed: toggleGlasses,
                 child:
                     Text(areGlassesVisible ? 'Remove Glasses' : 'Add Glasses'),
+              ),
+              ElevatedButton(
+                onPressed: toggleBlackEye,
+                child: Text(
+                    isBlackEyeVisible ? 'Remove Black Eye' : 'Add Black Eye'),
               ),
               ElevatedButton(
                 onPressed: toggleCrowns,
@@ -309,8 +344,9 @@ class _MyAppState extends State<MyApp> {
               ),
               ElevatedButton(
                 onPressed: toggleNeckBrace,
-                child: Text(
-                    isNeckBraceVisible ? 'Remove Neck Brace' : 'Add Neck Brace'),
+                child: Text(isNeckBraceVisible
+                    ? 'Remove Neck Brace'
+                    : 'Add Neck Brace'),
               ),
             ],
           ),
