4bc470354249bfb2b3a664c1f93e0b6715ef8efe
tmont5 <montgomerytanner40@gmail.com>
2023-07-12 12:41:17 -0600

    updated names in the values.dart file

--- lib/values.dart
+++ lib/values.dart
@@ -1,103 +1,102 @@
 import 'package:flutter/material.dart';
 import 'main.dart';
 
-Map<String, double> valuesGreen = {};
-Map<String, double> valuesBlue = {};
+Map<String, double> values2 = {};
+Map<String, double> values4 = {};
 Map<String, dynamic> images = {};
 void Maps() {
-  //probably want to chagne the names of these lists. like maybe values1 for 1.png and values 2 for 2.png
-  valuesGreen['glasses-offset-1'] = 0.0;
-  valuesGreen['glasses-offset-2'] = -5.0;
-  valuesGreen['glasses-width'] = 100;
-  valuesGreen['glasses-height'] = 100;
+  values2['glasses-offset-1'] = 0.0;
+  values2['glasses-offset-2'] = -5.0;
+  values2['glasses-width'] = 100;
+  values2['glasses-height'] = 100;
 
-  valuesGreen['crown-offset-1'] = -40.0;
-  valuesGreen['crown-offset-2'] = 35.0;
-  valuesGreen['crown-width'] = 20;
-  valuesGreen['crown-height'] = 25;
+  values2['crown-offset-1'] = -40.0;
+  values2['crown-offset-2'] = 35.0;
+  values2['crown-width'] = 20;
+  values2['crown-height'] = 25;
 
-  valuesGreen['earring-left-offset-1'] = -65.0;
-  valuesGreen['earring-left-offset-2'] = 15.0;
-  valuesGreen['earring-left-width'] = 20;
-  valuesGreen['earring-left-height'] = 25;
+  values2['earring-left-offset-1'] = -65.0;
+  values2['earring-left-offset-2'] = 15.0;
+  values2['earring-left-width'] = 20;
+  values2['earring-left-height'] = 25;
 
-  valuesGreen['earring-right-offset-1'] = 55.0;
-  valuesGreen['earring-right-offset-2'] = 15.0;
-  valuesGreen['earring-right-width'] = 20;
-  valuesGreen['earring-right-height'] = 25;
+  values2['earring-right-offset-1'] = 55.0;
+  values2['earring-right-offset-2'] = 15.0;
+  values2['earring-right-width'] = 20;
+  values2['earring-right-height'] = 25;
 
-  valuesGreen['neckbrace-offset-1'] = -5.0;
-  valuesGreen['neckbrace-offset-2'] = 80.0;
-  valuesGreen['neckbrace-width'] = 150;
-  valuesGreen['neckbrace-height'] = 150;
+  values2['neckbrace-offset-1'] = -5.0;
+  values2['neckbrace-offset-2'] = 80.0;
+  values2['neckbrace-width'] = 150;
+  values2['neckbrace-height'] = 150;
 
-  valuesGreen['black-eye-offset-1'] = -13.0;
-  valuesGreen['black-eye-offset-2'] = -2.0;
-  valuesGreen['black-eye-width'] = 100;
-  valuesGreen['black-eye-height'] = 100;
+  values2['black-eye-offset-1'] = -13.0;
+  values2['black-eye-offset-2'] = -2.0;
+  values2['black-eye-width'] = 100;
+  values2['black-eye-height'] = 100;
 
-  valuesGreen['eye-patch-offset-1'] = -30.0;
-  valuesGreen['eye-patch-offset-2'] = -10.0;
-  valuesGreen['eye-patch-width'] = 75;
-  valuesGreen['eye-patch-height'] = 75;
+  values2['eye-patch-offset-1'] = -30.0;
+  values2['eye-patch-offset-2'] = -10.0;
+  values2['eye-patch-width'] = 75;
+  values2['eye-patch-height'] = 75;
 
-  valuesGreen['tie-offset-1'] = -5.0;
-  valuesGreen['tie-offset-2'] = 110.0;
-  valuesGreen['tie-width'] = 100;
-  valuesGreen['tie-height'] = 100;
+  values2['tie-offset-1'] = -5.0;
+  values2['tie-offset-2'] = 110.0;
+  values2['tie-width'] = 100;
+  values2['tie-height'] = 100;
 
-  valuesGreen['pimple-offset-1'] = -30.0;
-  valuesGreen['pimple-offset-2'] = 40.0;
-  valuesGreen['pimple-width'] = 15;
-  valuesGreen['pimple-height'] = 20;
+  values2['pimple-offset-1'] = -30.0;
+  values2['pimple-offset-2'] = 40.0;
+  values2['pimple-width'] = 15;
+  values2['pimple-height'] = 20;
 
-  valuesBlue['glasses-offset-1'] = 0.0;
-  valuesBlue['glasses-offset-2'] = -15.0;
-  valuesBlue['glasses-width'] = 100;
-  valuesBlue['glasses-height'] = 100;
+  values4['glasses-offset-1'] = 0.0;
+  values4['glasses-offset-2'] = -15.0;
+  values4['glasses-width'] = 100;
+  values4['glasses-height'] = 100;
 
-  valuesBlue['crown-offset-1'] = 0;
-  valuesBlue['crown-offset-2'] = -100;
-  valuesBlue['crown-width'] = 100;
-  valuesBlue['crown-height'] = 100;
+  values4['crown-offset-1'] = 0;
+  values4['crown-offset-2'] = -100;
+  values4['crown-width'] = 100;
+  values4['crown-height'] = 100;
 
-  valuesBlue['earring-left-offset-1'] = -60.0;
-  valuesBlue['earring-left-offset-2'] = 15.0;
-  valuesBlue['earring-left-width'] = 20;
-  valuesBlue['earring-left-height'] = 25;
+  values4['earring-left-offset-1'] = -60.0;
+  values4['earring-left-offset-2'] = 15.0;
+  values4['earring-left-width'] = 20;
+  values4['earring-left-height'] = 25;
 
-  valuesBlue['earring-right-offset-1'] = 50.0;
-  valuesBlue['earring-right-offset-2'] = 0.0;
-  valuesBlue['earring-right-width'] = 20;
-  valuesBlue['earring-right-height'] = 25;
+  values4['earring-right-offset-1'] = 50.0;
+  values4['earring-right-offset-2'] = 0.0;
+  values4['earring-right-width'] = 20;
+  values4['earring-right-height'] = 25;
 
-  valuesBlue['neckbrace-offset-1'] = 0.0;
-  valuesBlue['neckbrace-offset-2'] = 100.0;
-  valuesBlue['neckbrace-width'] = 150;
-  valuesBlue['neckbrace-height'] = 150;
+  values4['neckbrace-offset-1'] = 0.0;
+  values4['neckbrace-offset-2'] = 100.0;
+  values4['neckbrace-width'] = 150;
+  values4['neckbrace-height'] = 150;
 
-  valuesBlue['black-eye-offset-1'] = -8.0;
-  valuesBlue['black-eye-offset-2'] = -8.0;
-  valuesBlue['black-eye-width'] = 100;
-  valuesBlue['black-eye-height'] = 100;
+  values4['black-eye-offset-1'] = -8.0;
+  values4['black-eye-offset-2'] = -8.0;
+  values4['black-eye-width'] = 100;
+  values4['black-eye-height'] = 100;
 
-  valuesBlue['eye-patch-offset-1'] = -25.0;
-  valuesBlue['eye-patch-offset-2'] = -20.0;
-  valuesBlue['eye-patch-width'] = 75;
-  valuesBlue['eye-patch-height'] = 75;
+  values4['eye-patch-offset-1'] = -25.0;
+  values4['eye-patch-offset-2'] = -20.0;
+  values4['eye-patch-width'] = 75;
+  values4['eye-patch-height'] = 75;
 
-  valuesBlue['tie-offset-1'] = 0.0;
-  valuesBlue['tie-offset-2'] = 120.0;
-  valuesBlue['tie-width'] = 100;
-  valuesBlue['tie-height'] = 100;
+  values4['tie-offset-1'] = 0.0;
+  values4['tie-offset-2'] = 120.0;
+  values4['tie-width'] = 100;
+  values4['tie-height'] = 100;
 
-  valuesBlue['pimple-offset-1'] = -10.0;
-  valuesBlue['pimple-offset-2'] = 70.0;
-  valuesBlue['pimple-width'] = 20;
-  valuesBlue['pimple-height'] = 25;
+  values4['pimple-offset-1'] = -10.0;
+  values4['pimple-offset-2'] = 70.0;
+  values4['pimple-width'] = 20;
+  values4['pimple-height'] = 25;
 
-  images['lib/images/people/1.png'] = valuesGreen; //valuesGreen;
-  images['lib/images/people/2.png'] = valuesGreen;
-  images['lib/images/people/3.png'] = valuesBlue;
-  images['lib/images/people/4.png'] = valuesBlue;
+  images['lib/images/people/1.png'] = values2; //valuesGreen;
+  images['lib/images/people/2.png'] = values2;
+  images['lib/images/people/3.png'] = values4;
+  images['lib/images/people/4.png'] = values4;
 }
